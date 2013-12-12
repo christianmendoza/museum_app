e8644444f44dc1d1e839ce9e007f5f75fa1e3445
Christian Mendoza <christian.mendoza@gmail.com>
2013-12-11 21:23:36 -0500

    Update painting controller

--- app/controllers/painting_controller.rb
+++ app/controllers/painting_controller.rb
@@ -2,4 +2,19 @@ class PaintingController < ApplicationController
   def index
     @paintings = Painting.all
   end
+
+  def new
+    @museums = Museum.all 
+    @artists = Artist.all
+    @painting = Painting.new
+  end
+
+  def create
+  end
+
+  private
+
+  def safe_painting 
+    params.require(:painting).permit(:title, :description, :image, :artist_id, :museum_id)
+  end
 end

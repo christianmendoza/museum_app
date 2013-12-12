df5e2e3bf578b0dd2d4b4b7d70fabadd8e57309b
Christian Mendoza <christian.mendoza@gmail.com>
2013-12-11 20:16:16 -0500

    Added artist controller and resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MuseumApp::Application.routes.draw do
+  get "artist/index"
   get "painting/index"
   root "museum#index"
+
+  resources :artists, only: [:show, :index, :new, :create, :update, :edit]
 end
